251c97aca2914346d4b7c3e57d7833d635d57f75
GoPetr <gopetr@gmail.com>
2023-03-31 00:32:11 +0400

    Add new class

---
+++ src/main/java/com/example/ClearWeather/ClearWeatherApplication.java
@@ -0,0 +1,13 @@
+package com.example.ClearWeather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClearWeatherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClearWeatherApplication.class, args);
+	}
+
+}
